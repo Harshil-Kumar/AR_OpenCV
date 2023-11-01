da40cb7d8b34a94b99ca3a589ade7ec2148b1db3
D Harshil Kumar Reddy <114159242+Harshil-Kumar@users.noreply.github.com>
2023-11-01 10:37:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250) 
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
+running = cv2.imread('running.png')
+interrupted = cv2.imread('interrupted.png')
+done = cv2.imread('done.png')
+
+cap = cv2.VideoCapture(0)
+
+import timer
+
+duration_string = input("Enter time for the timer (ex: 10m30s): ")
+seconds = timer.get_seconds_from_input(duration_string)
+
+timer.timer(seconds)
+
+print("Timer started for " + timer.pretty_time_from_seconds(seconds))
+
+while True:
+    ret, frame = cap.read()
+    from timer import timer_done
+    
+    corners, ids, rejectedImgPoints = detector.detectMarkers(frame)
+    if ids is not None:
+        for marker_id, marker_corners in zip(ids, corners):
+
+            if timer_done == 0:
+                image_to_overlay = running
+            elif timer_done == 1:
+                image_to_overlay = done
+            elif timer_done == 2:
+                image_to_overlay = interrupted
+            
+            # print(marker_id)
+            # print(list(marker_corners[0]))
+
+            marker_size = int(marker_corners[0][1][0] - marker_corners[0][0][0])
+
+            # print(marker_size)
+
+            mask = np.zeros(frame.shape, dtype=np.uint8)
+            cv2.fillPoly(mask, np.int32([marker_corners]), (255, 255, 255))
+            try:
+                overlay_resized = cv2.resize(image_to_overlay, (marker_size, marker_size))
+            except cv2.error:
+                continue
+
+            marker_width = 100  
+
+            overlay_width = image_to_overlay.shape[1]
+            overlay_height = image_to_overlay.shape[0]
+            dst_points = np.array([[0, 0], [overlay_width, 0], [overlay_width, overlay_height], [0, overlay_height]], dtype=np.float32)
+            perspective_transform = cv2.getPerspectiveTransform(dst_points, marker_corners)
+
+            overlay_warped = cv2.warpPerspective(image_to_overlay, perspective_transform, (frame.shape[1], frame.shape[0]))
+
+            # print(overlay_resized.shape, overlay_warped.shape)
+
+            alpha = 0.4
+            frame = cv2.addWeighted(frame, alpha, overlay_warped, 1-alpha, 1)
+
+
+    cv2.imshow('ArUco Marker with Overlay', frame)
+
+    waitkey = cv2.waitKey(1) & 0xFF
+    if waitkey == ord('q'):
+        timer.stop_timer()
+        print("Timer stopped.")
+        break
+    elif waitkey == ord('r'):
+        print("Timer reset.")
+        timer.set_timer_interrupted()
+    
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ timer.py
@@ -0,0 +1,85 @@
+import threading
+
+# initialize timer_done to False
+timer_done = 0
+lock = threading.Lock()
+
+timer_thread = None
+
+def set_timer_done():
+    global timer_done
+
+    if timer_done == 2:
+        return
+
+    lock.acquire()
+    timer_done = 1
+    lock.release()
+
+def set_timer_interrupted():
+    global timer_done
+    
+    lock.acquire()
+    timer_done = 2
+    lock.release()
+
+def timer(seconds):
+    global timer_thread
+    timer_thread = threading.Timer(seconds, lambda: set_timer_done())
+    timer_thread.start()
+
+def stop_timer():
+    global timer_thread
+    timer_thread.cancel()
+
+
+def get_seconds_from_input(input_time_str: str):
+    """Thanks to CorpNewt for helping out with this function"""
+    accepted_chars = {
+        "w": 604_800,
+        "d": 86_400,
+        "h": 3_600,
+        "m": 60,
+        "s": 1
+    }
+    time_seconds = 0
+    last_number = ""
+    for char in input_time_str:
+        if char.isdigit():  
+            last_number += char
+        elif char in accepted_chars:  
+            if last_number == "":
+                continue
+            time_seconds += int(last_number) * accepted_chars[char]
+            last_number = ""
+        else:
+            last_number = ""
+    if last_number:  
+        time_seconds += int(last_number)
+    return time_seconds
+
+def pretty_time_from_seconds(time_remaining: int):
+    if time_remaining < 0:
+        return "0 seconds"
+    minutes, seconds = divmod(time_remaining, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+
+    final_string_to_join = []
+    if weeks > 0:
+        final_string_to_join.append(f"{weeks} {'weeks' if weeks != 1 else 'week'}")
+    if days > 0:
+        final_string_to_join.append(f"{days} {'days' if days != 1 else 'day'}")
+    if hours > 0:
+        final_string_to_join.append(f"{hours} {'hours' if hours != 1 else 'hour'}")
+    if minutes > 0:
+        final_string_to_join.append(f"{minutes} {'minutes' if minutes != 1 else 'minute'}")
+    if seconds > 0:
+        final_string_to_join.append(f"{seconds} {'seconds' if seconds != 1 else 'second'}")
+
+    if len(final_string_to_join) > 1:
+        final_string = ", ".join(final_string_to_join[:-1]) + f", and {final_string_to_join[-1]}"
+    else:
+        final_string = ", ".join(final_string_to_join)
+    return final_string
